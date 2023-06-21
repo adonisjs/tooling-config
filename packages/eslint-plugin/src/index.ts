af9e306d716b0694d9c4552f42993e14bb027751
Julien <julien@ripouteau.com>
2023-06-21 11:26:06 +0200

    fix: add missing rule

--- packages/eslint-plugin/src/index.ts
+++ packages/eslint-plugin/src/index.ts
@@ -1,7 +1,9 @@
 import preferLazyControllerImport from './rules/prefer_lazy_controller_import'
+import preferLazyListenerImport from './rules/prefer_lazy_listener_import
 
 export = {
   rules: {
     'prefer-lazy-controller-import': preferLazyControllerImport,
+    'prefer-lazy-listener-import': preferLazyListenerImport,
   },
 }
